50e509e7be7a7cdc1b7c339697d8f25856e3861d
RicardoCalvoP <a01028889@tec.mx>
2024-02-24 16:08:32 -0600

    practice

--- Web/Tareas/javascript_basics/Ejercicio8.js
+++ Web/Tareas/javascript_basics/Ejercicio8.js
@@ -5,40 +5,20 @@ Ricardo Calvo - A01028889
 Write a function that removes duplicate elements from an array and returns a list of the remaining elements. For example: removeDuplicates([1, 0, 1, 1, 0, 0]) -> [1, 0]
 */
 
-let numbers = [1, 1, 2, 2, 3, 3]
-let length = numbers.length
+let numbers = [1, 1, 2, 2, 3, 3, 4, 2, 4, 5, 6, 4, 3, 4, 4, 2, 3, 1]
 
-for (let i = 0; i < length; i++) {
-	for (let j = 0; j < length; j++) {
-		if (i == j)
-			numbers.pop(i)
-	}
-}
-
-console.log(numbers)
-
-/*
-function DeleteRepeat(numbers, numbers2, length) {
-
+function RemoveRepeated(numbers) {
+	let seen = {}
+	let result = []
+	let length = numbers.length
 	for (let i = 0; i < length; i++) {
-		for (let j = numbers[i]; j < length; j++) {
-			if (j != numbers2[j]) {
-				numbers2.push(j)
-				console.log(numbers2)
-			}
+		if (!seen[numbers[i]]) {
+			result.push(numbers[i])
+			seen[numbers[i]] = true
 		}
 	}
+	return result
 }
 
-console.log(DeleteRepeat(numbers, numbers2, length))
-
-/*
-	for (let k = 0; k < Position.length; k++) {
-		numbers.splice(Position[k], 1)
-	}
-	return numbers
-}
-
-console.log('Original list: ', numbers)
-console.log(DeleteRepeat('Non repited numbers: ', numbers))
-*/
+console.log('Original array: ', numbers)
+console.log('Duplicates removed: ', RemoveRepeated(numbers))
