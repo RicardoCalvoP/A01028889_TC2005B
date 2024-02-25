08bb262a77025dcda4d932f468a040685c474681
RicardoCalvoP <a01028889@tec.mx>
2024-02-25 11:26:06 -0600

    Practice v1

---
+++ Web/Tareas/javascript_basics/Ejercicio9.js
@@ -0,0 +1,8 @@
+/*
+Ricardo Calvo - A01028889
+24/02/2024
+
+Write a function that takes a list of text strings as a parameter, and returns the length of the shortest string.
+*/
+
+let list = ['this', 'is', 'a', 'string']
